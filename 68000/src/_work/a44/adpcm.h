6a26c57d96cd83cf61321d17332eb085e123fb6a
tantanGH <tantanGH>
2025-09-16 17:36:05 +0900

    update

--- 68000/src/_work/a44/adpcm.h
+++
@@ -1,7 +0,0 @@
-void _ptoa_make_buffer( void );
-void _ptoa_init( short );
-void _ptoa_exec( unsigned int, short *, unsigned char * );
-void _atop_make_buffer( char * );
-void _atop_init( short );
-void _atop_exec( unsigned int, unsigned char *, short * );
-
